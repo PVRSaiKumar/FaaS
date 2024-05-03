da9a3222278939397dddaf535d95e2574cf9f501
PVRSaiKumar <pvrsaikumar2004@gmail.com>
2024-05-03 15:58:56 +0530

    updated client

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import requests
 myobj = {'somekey': 'somevalue'}
 file={"myfile":open("Example_codes/Dockerfile","r"),"myfile2":open("Dind_Dockerfile","r")}
-# r=requests.get("http://localhost:8000/idkyaar",params=myobj,files=file)
-r=requests.post("http://localhost:8000/idkyaar2",data=myobj,files=file)
+r=requests.get("http://{IP_Kubernetes}:31001/trigger_url",params=myobj,files=file)
+r=requests.post("http://{IP_Kubernetes}:31001/trigger_url",data=myobj,files=file)
 with open('downloaded_file_client.zip', 'wb') as f:  # Adjust filename and mode as needed
     f.write(r.content)
     print('File downloaded successfully.')
