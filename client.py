5d9796261fe33eb1144719393d1d8774b3d52f24
PVRSaiKumar <pvrsaikumar2004@gmail.com>
2024-05-03 15:59:18 +0530

    updated client

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import requests
+IP_kubernetes = "10.129.2.183"
 myobj = {'somekey': 'somevalue'}
 file={"myfile":open("Example_codes/Dockerfile","r"),"myfile2":open("Dind_Dockerfile","r")}
-r=requests.get("http://{IP_Kubernetes}:31001/trigger_url",params=myobj,files=file)
-r=requests.post("http://{IP_Kubernetes}:31001/trigger_url",data=myobj,files=file)
+r=requests.get(f"http://{IP_kubernetes}:31001/trigger_url",params=myobj,files=file)
+r=requests.post(f"http://{IP_kubernetes}:31001/trigger_url",data=myobj,files=file)
 with open('downloaded_file_client.zip', 'wb') as f:  # Adjust filename and mode as needed
     f.write(r.content)
     print('File downloaded successfully.')
