51d8d35647038fcc5256cdaec9a1832abaf1f357
PVRSaiKumar <pvrsaikumar2004@gmail.com>
2024-05-04 06:04:29 +0530

    updated handler

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-IP_kubernetes = "192.128.49.2"
+IP_kubernetes = "192.168.49.2"
 # myobj = {'somekey': 'somevalue'}
 file={"myfile":open("actual.png","rb")}#,"myfile2":open("Dind_Dockerfile","r")}
 # r=requests.get(f"http://{IP_kubernetes}:32001/trigger_url",params=myobj,files=file)
--- handler.py
+++ handler.py
@@ -9,6 +9,7 @@ triggers = 'Data/trigger.json'
 CONT_PORT = 9000
 HANDLER_PORT = 31001
 SERVER_PORT = 31000
+SERVICE_PORT = 29000
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''}, methods=['GET','POST'])
@@ -17,10 +18,10 @@ def catch_all(path):
 	trigger_data = json.load(open(triggers,'r'))
 	function_data = json.load(open(functions,'r'))
 	for fn_name in trigger_data.keys():
-		for triggers in trigger_data[fn_name]:
-			if triggers[0] == path and triggers[1] == request.method:
+		for trigger in trigger_data[fn_name]:
+			if trigger[0] == path and trigger[1] == request.method:
 				if request.method=="GET":
-					r=requests.post(f"http://{function_data[fn_name][2]}:{CONT_PORT}/",data={"ARGS":json.dumps(request.args),"COMMAND":triggers[2],"RETURN":triggers[3]},files=request.files)
+					r=requests.post(f"http://{function_data[fn_name][2]}:{SERVICE_PORT}/",data={"ARGS":json.dumps(request.args),"COMMAND":trigger[2],"RETURN":trigger[3]},files=request.files)
 
 					zip_filename = 'downloaded_file.zip'
 					with open(zip_filename, 'wb') as f:
@@ -31,7 +32,7 @@ def catch_all(path):
 					return response
 
 				elif request.method=="POST":
-					r=requests.post(f"http://{function_data[fn_name][2]}:{CONT_PORT}/",data={"ARGS":json.dumps(request.form),"COMMAND":triggers[2],"RETURN":triggers[3]},files=request.files)
+					r=requests.post(f"http://{function_data[fn_name][2]}:{SERVICE_PORT}/",data={"ARGS":json.dumps(request.form),"COMMAND":trigger[2],"RETURN":trigger[3]},files=request.files)
 
 					zip_filename = 'downloaded_file.zip'
 					with open(zip_filename, 'wb') as f:
@@ -40,6 +41,7 @@ def catch_all(path):
 					response = make_response(send_from_directory('', zip_filename, as_attachment=True))
 					response.headers['Content-Type'] = 'application/zip'
 					return response
+	return "trigger not registered"
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0',port=HANDLER_PORT)
